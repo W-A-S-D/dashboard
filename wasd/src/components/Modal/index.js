ae3cfbed40d6968181fa2481060a669e75458cd1
Manocchio <guruu.leste@gmail.com>
2021-10-16 18:30:22 -0300

    Input components

--- wasd/src/components/Modal/index.js
+++ wasd/src/components/Modal/index.js
@@ -3,6 +3,7 @@ import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Modal from '@material-ui/core/Modal';
+import ModalInput from '../ModalInput'
 import {styles} from './style.js'; 
 
 const ModalComponent = (props) => {
@@ -15,14 +16,16 @@ const ModalComponent = (props) => {
                 aria-describedby="modal-modal-description"
             >
                 <Box sx={styles.boxContainer}>
-                    <div style={{display:'flex',flexDirection:'column' ,justifyContent: 'center', alignItems:'center'}}>
+                    <div style={{display:'flex',flexDirection:'column' ,justifyContent: 'center', alignItems:'center', marginBottom: 45}}>
                         <h1 style={{marginBottom: 2, marginTop: 5}}>Cadastro de Funcionário</h1>
                         <div style={{ height: '2px', width: '85%', backgroundColor: 'rgba(136, 136, 136, 0.54)'}}></div>
                     </div>
-                    <div style={{margin:'35px 32px'}}>
-                        <div style={{color: 'rgba(0, 0, 0, 0.4)', marginBottom: '15px'}}>props.label</div>
-                        <input style={{padding:'0 15px',minHeight:'54px',borderRadius: 10,outline:'none' ,border: 0, boxShadow:'4px 4px 8px rgba(0, 0, 0, 0.24)'}} security={props.security} type="text" />
-                    </div>
+
+                  <ModalInput width={'100%'} label="Nome do Funcionário" />
+                  <ModalInput width={'100%'} label="Senha" pass={true} />
+                  <ModalInput width={'100%'} label="Setor" />
+                  
+                  
                 </Box>
             </Modal>
         </>
